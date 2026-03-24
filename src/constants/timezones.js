c84d04e88d83b55b097615c0caa420cf44459626
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2026-03-24 12:42:40 -0400

    small fix

--- src/constants/timezones.js
+++ src/constants/timezones.js
@@ -21,7 +21,7 @@ export const USERS = {
 		id: "appel",
 		name: "Appel",
 		person: "Appel",
-		city: "A deux",
+		city: "",
 		flag: "\uD83D\uDFE2",
 		zone: "America/Toronto",
 		color: "#9fe8b1",
